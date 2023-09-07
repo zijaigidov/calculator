9bb66033a46f2d5e816a6e6d5b556522f609d902
zijaigidov <igidov.zija@gmail.com>
2023-09-07 14:12:13 +0200

    Add functions for math operations

---
+++ script.js
@@ -0,0 +1,16 @@
+// MATH OPERATIONS
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return b === 0 ? 'ERROR: Division by zero' : a / b;
+}
